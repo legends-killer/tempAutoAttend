5418adab2c38ec8a051e11bc13bf1157f82886c1
legends-killer <958775729@qq.com>
2020-02-13 11:23:03 +0800

    add: log record and remove cmd lines.

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ import pyautogui as pg
 import pytweening
 import datetime
 import os
-
+ISOTIMEFORMAT_F = '%H%M'
+log = '%Y-%m-%d %H:%M'
+last_time = "0000"
 def my_click():
     width, height = pg.size()
     print(width ,height)
@@ -23,23 +25,37 @@ def main():
     my_click()
     sleep(5)
 
-ISOTIMEFORMAT_F = '%H%M'
-log = '%Y-%m-%d %H:%M'
-last_time = "0000"
+
 print("自动签到已启动,上次签到于：" + last_time)
+file = open('log' + '.txt','w')
+file.close()
+file = open('time' + '.txt','w')
+file.close()
 cnt = 0
 while 1:
     cnt = cnt+1
     if cnt >= 10:
         cnt = 0
-        os.system("cls")
-        print("自动签到已启动,上次签到于：" + last_time)
+        f = open('time.txt','r+')
+        f.truncate()
     sleep(1)
     time = datetime.datetime.now().strftime(ISOTIMEFORMAT_F)
     print("当前时间:" + time)
+    with open('time.txt','w') as f:
+        f.write('当前时间')
+        f.write(time)
+        f.write('\n')
     if time >= '0825' and time <= '0835':
         last_time = datetime.datetime.now().strftime(log)
         main()
+        with open('log.txt','w') as f:
+            f.write('自动签到成功！于：')
+            f.write(last_time)
+            f.write('\n')
     if time >= '2125' and time <= '2135':
         last_time = datetime.datetime.now().strftime(log)
         main()
+        with open('log.txt','w') as f:
+            f.write('自动签到成功！于：')
+            f.write(last_time)
+            f.write('\n')
